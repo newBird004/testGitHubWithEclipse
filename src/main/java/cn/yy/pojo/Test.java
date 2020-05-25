aa88bc999e068c52229a579173700e392fb4e7e1
tom_glb <goodMorning_glb@qq.com>
2020-05-25 12:12:20 +0800

    first commit with eclipse

---
+++ src/main/java/cn/yy/pojo/Test.java
@@ -0,0 +1,16 @@
+package cn.yy.pojo;
+
+
+public class Test {
+
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
